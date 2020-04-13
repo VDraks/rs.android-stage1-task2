3c756fdc54895923032bfe43067a137a2a550d63
VDraks <kytinvasya@gmail.com>
2020-04-13 23:11:13 +0300

    feat: implement Blocks

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,29 @@
 package subtask5
 
+import java.lang.Exception
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        var intRes: Int = 0
+        var stringRes: String = ""
+        var dateRes: LocalDate = LocalDate.MIN
+        blockA.forEach {
+            when (it) {
+                is Int -> intRes += it
+                is String -> stringRes += it
+                is LocalDate -> dateRes = maxOf(dateRes, it)
+            }
+        }
+
+        when (blockB) {
+            Int::class -> return intRes
+            String::class -> return stringRes
+            LocalDate::class -> return dateRes.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            else -> throw Exception()
+        }
     }
 }
