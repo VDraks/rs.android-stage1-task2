d732772829630c8fa1ce0aa6332cbea5f0341c52
VDraks <kytinvasya@gmail.com>
2020-04-13 18:58:13 +0300

    feat: implement toTextDay

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,22 @@
 package subtask1
 
+import java.lang.Exception
+import java.util.Locale
+import java.util.Calendar
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val locale = Locale("ru")
+        val c = Calendar.getInstance()
+        c.isLenient = false
+        try {
+            c.set(year.toInt(), month.toInt() - 1, day.toInt())
+            val monthText = c.getDisplayName(Calendar.MONTH, Calendar.LONG, locale)
+            val dayOfTheWeekText = c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, locale)
+            return day + " " + monthText + ", " + dayOfTheWeekText
+        } catch (e: Exception) {
+            return "Такого дня не существует"
+        }
     }
 }
