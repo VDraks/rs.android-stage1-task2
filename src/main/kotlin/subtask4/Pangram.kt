28043a7171b51c903a767d5e3b6001a394164e06
VDraks <kytinvasya@gmail.com>
2020-04-13 22:41:10 +0300

    feat: implement Pangram

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -1,9 +1,53 @@
 package subtask4
 
+import java.util.*
+import java.util.regex.Pattern
+import kotlin.collections.HashSet
+
 class Pangram {
 
-    // TODO: Complete the following function
+    private val alphabetStart = 97
+    private val alphabetEnd = 122
+
+    private fun isLetter(char: Char): Boolean {
+        return char.toLowerCase().toInt() in alphabetStart..alphabetEnd
+    }
+
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val alphabet = HashSet<Char>()
+        for (i in alphabetStart until alphabetEnd + 1) {
+            alphabet.add(i.toChar())
+        }
+        inputString.forEach { c -> alphabet.remove(c.toLowerCase()) }
+
+        val isPangram = alphabet.isEmpty()
+
+        val vowels = HashSet<Char>()
+        vowels.addAll(arrayOf('a', 'e', 'i', 'o', 'u', 'y'))
+
+        val charIsTarget: (Char) -> Boolean = { c ->
+            val lowerC = c.toLowerCase()
+            if (isPangram) {
+                vowels.contains(lowerC)
+            } else {
+                !vowels.contains(lowerC) && isLetter(lowerC)
+            }
+        }
+
+        val words = inputString.split(Pattern.compile("\\s+")).filter { s -> s.isNotEmpty() }
+
+        val sortedMap = TreeMap<Int, MutableList<String>>()
+
+        words.forEach { word ->
+            val count = word.count { c -> charIsTarget(c) }
+            val convertedWord = word.map { c -> if (charIsTarget(c)) c.toUpperCase() else c }.joinToString("")
+            val finalWord = count.toString() + convertedWord
+            if (sortedMap[count] == null) sortedMap[count] = mutableListOf(finalWord)
+            else sortedMap[count]?.add(finalWord)
+        }
+
+        val result = sortedMap.toList().joinToString(separator = " ", transform = { pair -> pair.second.joinToString(" ") })
+        return result
     }
 }
