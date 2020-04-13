2d47eda29fd603eb44f47286ace8b6cba6abaca0
VDraks <kytinvasya@gmail.com>
2020-04-13 20:45:32 +0300

    feat: implement TimeConverter

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -1,9 +1,43 @@
 package subtask2
 
 class TimeConverter {
+    private val unitsArray = arrayOf(
+        "zero", "one", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine", "ten", "eleven", "twelve",
+        "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+        "eighteen", "nineteen"
+    )
+    private val tensArray = arrayOf(
+        "zero", "ten", "twenty", "thirty", "forty", "fifty"
+    )
 
-    // TODO: Complete the following function
+    private fun minutesToText(minute: Int): String {
+        return when (minute) {
+            0 -> unitsArray[0]
+            in 1..19 -> unitsArray[minute]
+            else -> tensArray[minute / 10] + " " + unitsArray[minute % 10]
+        }
+    }
+
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val hourInt = hour.toInt()
+        val minuteInt = minute.toInt()
+
+        val minuteText: () -> String = { minutesToText(minuteInt) }
+        val hourText: () -> String = { unitsArray[hourInt] }
+        val reverseMinuteText: () -> String = { minutesToText(60 - minuteInt) }
+        val nextHourText: () -> String = { unitsArray[hourInt % 12 + 1] }
+
+        return when (minuteInt) {
+            0 -> hourText() + " o' clock"
+            1 -> minuteText() + " minute past " + hourText()
+            15 -> "quarter past " + hourText()
+            30 -> "half past " + hourText()
+            45 -> "quarter to " + nextHourText()
+            59 -> reverseMinuteText() + " minute to " + nextHourText()
+            in 1..30 -> minuteText() + " minutes past " + hourText()
+            in 31..59 -> reverseMinuteText() + " minutes to " + nextHourText()
+            else -> ""
+        }
     }
 }
